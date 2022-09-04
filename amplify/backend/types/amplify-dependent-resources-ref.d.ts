cacae6f7ca830849d98f2646f11011ef0e115bc1
Austin <leahy@apache.org>
2022-09-03 22:41:30 -0600

    getting things working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "surveypwa1a7615c6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "analytics": {
+        "surveypwa": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    },
+    "api": {
+        "surveypwa": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "surveypwa1a7615c6PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string"
+        }
+    }
+}
